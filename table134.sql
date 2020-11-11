36ce40b54c9b5431ac7c4b38cb3ba77f41a26f62
saicharan <69844239+saicharansigiri@users.noreply.github.com>
2020-11-11 19:35:36 +0530

    Update table134.sql

--- table134.sql
+++ table134.sql
@@ -92,20 +92,42 @@ INSERT INTO T5_INSURANCE_COMPANY('HDFC Life Insurance Company Limited','Lodha Ex
 INSERT INTO T5_INSURANCE_COMPANY('ICICI Prudential Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai ',18602667766,02545465451,'iciciprulife@gmail.com','https://www.iciciprulife.com/','Mumbai',' ICICI PruLife',' ICICI PruLife');
 INSERT INTO T5_INSURANCE_COMPANY('Exide Life Insurance Company Limited','Registered Office: 3rd Floor, JP Techno Park, No. 3/1, Millers Road, Bangalore- 560 001, India.',180014141414,02215454515,'ExideLifeInsurance@gmail.com','https://www.exidelife.in/','Mumbai','exidlifeoffice','exidelife');
 INSERT INTO T5_INSURANCE_COMPANY('Life Insurance Corporation of India','2nd Floor Jagtial 505327',18005522552,021155779,'lic@gmail.com','www.lic.com','jagtial','licjagtial','licjag');
-INSERT INTO T5_INSURANCE_COMPANY(' Max Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY(' PNB MetLife India Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Kotak Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('SBI Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Tata AIA Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Reliance Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Aviva Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Shriram Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Bharti AXA Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY(' Future Generali India Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('IDBI Federal Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY(' Canara HSBC Oriental Bank of Commerce LIC Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('Aegon Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
-INSERT INTO T5_INSURANCE_COMPANY('DLF Pramerica Life Insurance Company Limited','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180020010020,556622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Max Life Insurance Company Limited','Max Life Insurance Co. Ltd., 3rd, 11th and 12th Floor, DLF Square Building, Jacaranda Marg, DLF City Phase II, Gurugram (Haryana) - 122002',18601205577,122622020,'maxlife@help.com','maxlife.com','maxlifeoffice','maxlifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('PNB MetLife India Insurance Company Limited','PNB Metife Insurance Company Limited 2nd Floor, Peninsula Tower, A Wing Peninsula Corp. Park, G. K. Marg Lower Parel, Mumbai - 400 013', Mumbai',1800754123127,5565454752020,'pnbmedlife@help.com','pnbmedlife.com','medlifeoffice','pnblifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Kotak Life Insurance Company Limited','Kotak Mahindra Life Insurance Company Limited, Regd. Office: 2nd Floor, Plot # C- 12, G- Block, BKC, Bandra (E), Mumbai - 400 051','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',1800287867160,556622020,'KotakLifeInsurance@help.com','KotakLifeInsurance.com','kotaklifeoffice','kotaklifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('SBI Life Insurance Company Limited','SBI Life Corporate Solutions. 7th Floor, "Natraj", MV Road & Western Express Highway Junction, Andheri (East) , Mumbai - 400 069','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',180571584020,5565871687,'sbilife@help.com','sbilife.com','sbilifeoffice','sbilifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Tata AIA Life Insurance Company Limited','Floor, Tower A, Peninsula Business Park, Senapati Bapat Marg, Lower Parel, Mumbai 400013','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',1881784560,555444220220,'tataaia@help.com','tatalife.com','tatalifeoffice','tatalifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Reliance Life Insurance Company Limited','Reliance Centre, 5th floor, Off Western Express Highway, Santacruz East, Mumbai, Mumbai-City district, Maharashtra-400055','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',18068168110020,55540542020,'reliancelife@help.com','reliancelife.com','reliancelifeoffice',reliancelifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Aviva Life Insurance Company Limited','Towers, Sector Road, Opposite Golf Course, DLF Phase V, Sector 43, Gurgaon – 122003.','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',1858406843500,5684620,'aiaamaxlife@help.com','aviamaxlife.com','aviamaxlifeoffice','aviamaxlifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Shriram Life Insurance Company Limited','Bank Training Centre, Financial District, Gachibowli, Hyderabad, TG, India – 500032.','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',165806805520,5568470640620,'Shriramlife@help.com','Shriramlife.com','Shriramlifeoffice','Shriramlifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Bharti AXA Life Insurance Company Limited','130, Unit No. 1904, 19th Floor, Parinee Crescenzo, 'G' Block, Bandra','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',18060565020,5505650022020,'Bhartilife@help.com','Bhartilife.com','Bhartilifeoffice','Bhartilifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY(' Future Generali India Life Insurance Company Limited','1st Floor, 7-1-21A, Raj Bhavan Rd, opp. Lifestyle Building, beside Vinn Hospital APDL Estates, Begumpet, Hyderabad, Telangana 500016','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',18060505087610020,556806820,'Futurelife@help.com','Futurelife.com','Futurelifeoffice','Futurelifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('IDBI Federal Life Insurance Company Limited','3-5-922 Sri Sai Balaji Residency, Near Narayanaguda Flyover, Hyderabad, Telangana 500029','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',6878605410620,68744545600,'IDBIFederallife@help.com','IDBIFederallife.com','IDBI Federallifeoffice','IDBIFederallifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY(' Canara HSBC Oriental Bank of Commerce LIC Limited','206 & 207, 2nd Floor, Aditya Trade Center, 7-1-618, Ameerpet, Hyderabad, Telangana 500038','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',6804764554,184556622020,'Canaralife@help.com','Canaralife.com','Canaralifeoffice','Canaralifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('Aegon Life Insurance Company Limited','206 & 207, 2nd Floor, Aditya Trade Center, 7-1-618, Ameerpet, Hyderabad, Telangana 500038','1089 Appasaheb Marathe Marg, Prabhadevi, Mumbai',1900010020,5566565220,'Aegonlife@help.com','Aegonlife.com','Aegonlifeoffice','Aegonlifeoffice');
+INSERT INTO T5_INSURANCE_COMPANY('DLF Pramerica Life Insurance Company Limited','Office No.501- 520, on 5th floor, Bezzola Complex C.H.S. Ltd., S. No. 237, C.S. No. 353, 352/2, 353/3, 353/4, Sion Trombay Road,',19010020010020,5566889020,'DLFlife@help.com','DLFlife.com','DLFlifeoffice','DLFlifeoffice');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
